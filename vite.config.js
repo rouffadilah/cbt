44e444e2e171230e92e7b391f61410114a08d3b0
Rouf Fadilah <rouf.learn@gmail.com>
2026-06-17 17:21:54 +0700

    update konfigurasi deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cbt/', // TAMBAHKAN BARIS INI! Sesuaikan dengan nama repository GitHub Anda
+  base: "/cbt/", // <-- Tambahkan baris ini!
 })
